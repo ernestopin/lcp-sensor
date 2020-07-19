16f4f528e8c8515c10882e26c13988632f653b1b
Ernesto Pin <ernestopin@gmail.com>
2020-07-19 14:24:02 -0300

    hardcoded and not that precise calibration

--- temperature-display/temperature_display.ino
+++ temperature-display/temperature_display.ino
@@ -21,6 +21,10 @@ Adafruit_7segment led[2] = {Adafruit_7segment(), Adafruit_7segment()};
 
 /* Globals */
 float temps[2] = {0, 0};
+float ref_low = 17;
+float ref_high = 33;
+float raw_high[2] = {33.63, 33.25};
+float raw_low[2] = {18.19, 17.94};
 
 void setup()
 {
@@ -43,8 +47,8 @@ void loop()
 
 void displayTemp(float read, int temp) {
 	if (temps[temp] != read) {
-		temps[temp] = read;
-		led[temp].print(read);
+		temps[temp] = (read - raw_low[temp]) * (ref_high - ref_low) / (raw_high[temp] - raw_low[temp]) + ref_low;
+		led[temp].print(temps[temp]);
 		led[temp].writeDisplay();
 	}
 }
